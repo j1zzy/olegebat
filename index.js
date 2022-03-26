949691dd549b647af279393c24adfe3a08443136
j1zzy <95938320+j1zzy@users.noreply.github.com>
2022-03-26 13:40:44 +0300

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,15 @@
+<component name="ProjectCodeStyleConfiguration">
+  <state>
+    <option name="PREFERRED_PROJECT_CODE_STYLE" value="Default" />
+  </state>
+</component>
+<component name="ProjectCodeStyleConfiguration">
+  <state>
+    <option name="PREFERRED_PROJECT_CODE_STYLE" value="Default" />
+  </state>
+</component>
+<component name="ProjectCodeStyleConfiguration">
+  <state>
+    <option name="PREFERRED_PROJECT_CODE_STYLE" value="Default" />
+  </state>
+</component>
